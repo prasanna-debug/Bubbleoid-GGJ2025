cebee6400f45716fff85405c316b0a3f00772b55
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-01-25 16:49:19 +0100

    updated the About Box description text

---
+++ src/uTxtAboutDescription.pas
@@ -0,0 +1,143 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Bubbleoid
+///
+/// Copyright 2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bubbleoid.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bubbleoid-GGJ2025
+///
+/// ***************************************************************************
+/// File last update : 2025-01-25T16:46:10.000+01:00
+/// Signature : 67f0a4805e45e4f132ce7fe5f915c635aaf4f4eb
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutDescription;
+
+interface
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean = false): string;
+
+// TODO : compléter la description et les crédits dans le texte
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const
+  CTxtEN = '''
+TODO
+
+*****************
+* Credits
+
+This application was developed by Patrick Prémartin in Delphi.
+
+TODO
+
+*****************
+* Publisher info
+
+This video game is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.
+
+****************
+* Personal data
+
+This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.
+
+We have no knowledge of what you do with it.
+
+No information about you is transmitted to us or to any third party.
+
+We use no cookies, no tracking, no stats on your use of the application.
+
+***************
+* User support
+
+If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.
+
+To find out more, visit https://bubbleoid.gamolf.fr
+
+''';
+   CTxtFR = '''
+TODO
+
+*****************
+* Remerciements
+
+Cette application a été développée par Patrick Prémartin en Delphi.
+
+TODO
+
+*****************
+* Info éditeur
+
+Ce jeu vidéo est éditée par OLF SOFTWARE, société enregistrée à Paris (France) sous la référence 439521725.
+
+****************
+* Données personnelles
+
+Ce programme est autonome dans sa version actuelle. Il ne dépend pas d'Internet et ne communique rien au monde extérieur.
+
+Nous n'avons aucune connaissance de ce que vous faites avec lui.
+
+Aucune information vous concernant n'est transmise à nous ou à des tiers.
+
+Nous n'utilisons pas de cookies, pas de tracking, pas de statistiques sur votre utilisation de l'application.
+
+***************
+* Assistance aux utilisateurs
+
+Si vous avez des questions ou si vous avez besoin de fonctionnalités supplémentaires, veuillez nous laisser un message sur le site web de l'application ou sur son dépôt de code.
+
+Pour en savoir plus, visitez https://bubbleoid.gamolf.fr
+
+''';
+  // CTxtIT = '';
+  // CTxtDE = '';
+  // CTxtJP = '';
+  // CTxtPT = '';
+  // CTxtES = '';
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if not Recursif then
+    result := GetTxtAboutDescription(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow description for language "' +
+      Language + '".');
+end;
+
+end.
