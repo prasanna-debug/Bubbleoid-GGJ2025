de12bdc7f91cba4de6a7cb23262d88eb9ae500af
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-01-25 19:45:16 +0100

    added a text button for the menu and dialog buttons

---
+++ src/cButtonText.pas
@@ -0,0 +1,110 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Bubbleoid
+///
+/// Copyright 2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bubbleoid.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bubbleoid-GGJ2025
+///
+/// ***************************************************************************
+/// File last update : 2025-01-25T19:36:54.000+01:00
+/// Signature : ee829e4f15b06f1b7c0dd36368f7962cc1105ed1
+/// ***************************************************************************
+/// </summary>
+
+unit cButtonText;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  _ButtonsAncestor,
+  Olf.FMX.TextImageFrame,
+  FMX.Objects,
+  FMX.Layouts;
+
+type
+  TButtonText = class(T__ButtonAncestor)
+    rBackground: TRectangle;
+    tiText: TOlfFMXTextImageFrame;
+    rFocused: TRectangle;
+    lText: TLayout;
+  private
+  protected
+    function GetImageIndexOfUnknowChar(Sender: TOlfFMXTextImageFrame;
+      AChar: char): integer;
+  public
+    procedure Repaint; override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  USVGAdobeStock,
+  uSVGBitmapManager_InputPrompts,
+  udmAdobeStock_497062500;
+
+function TButtonText.GetImageIndexOfUnknowChar(Sender: TOlfFMXTextImageFrame;
+  AChar: char): integer;
+begin
+  if (AChar = 'ç') then
+    result := Sender.getImageIndexOfChar('C')
+  else
+    result := Sender.getImageIndexOfChar(UpperCase(AChar));
+end;
+
+procedure TButtonText.Repaint;
+begin
+  if IsDown then
+    rBackground.fill.bitmap.bitmap.Assign
+      (getBitmapFromSVG(TSVGAdobeStockIndex.ButtonOn, rBackground.Width,
+      rBackground.Height, rBackground.fill.bitmap.bitmap.BitmapScale))
+  else
+    rBackground.fill.bitmap.bitmap.Assign
+      (getBitmapFromSVG(TSVGAdobeStockIndex.ButtonOff, rBackground.Width,
+      rBackground.Height, rBackground.fill.bitmap.bitmap.BitmapScale));
+  tiText.Font := dmAdobeStock_497062500.ImageList;
+  tiText.OnGetImageIndexOfUnknowChar := GetImageIndexOfUnknowChar;
+  tiText.Text := Text;
+  if IsFocused then
+  begin
+    rFocused.fill.bitmap.bitmap.Assign
+      (getBitmapFromSVG(TSVGAdobeStockIndex.LightningBlue, rFocused.Width,
+      rFocused.Height, rFocused.fill.bitmap.bitmap.BitmapScale));
+    rFocused.Visible := true;
+  end
+  else
+    rFocused.Visible := false;
+end;
+
+end.
