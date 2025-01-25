488a125025b14d7df9f24adabd2b72c40ed0fce3
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-01-25 18:43:46 +0100

    référencement des SVG dans le moteur d'affichage du projet

---
+++ src/uSVGBitmapManager_InputPrompts.pas
@@ -0,0 +1,90 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Bubbleoid
+///
+/// Copyright 2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://bubbleoid.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Bubbleoid-GGJ2025
+///
+/// ***************************************************************************
+/// File last update : 2025-01-25T18:42:00.000+01:00
+/// Signature : a100a874e74bafca9957ccb4493fa5cf4b7340e7
+/// ***************************************************************************
+/// </summary>
+
+unit uSVGBitmapManager_InputPrompts;
+
+interface
+
+uses
+  FMX.Graphics,
+  USVGInputPrompts,
+  USVGBubbleFont,
+  USVGAdobeStock;
+
+/// <summary>
+/// Returns a bitmap from a SVG image
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+function getBitmapFromSVG(const Index: TSVGAdobeStockIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+function getBitmapFromSVG(const Index: TSVGBubbleFontIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+implementation
+
+uses
+  Olf.Skia.SVGToBitmap;
+
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGInputPrompts.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+function getBitmapFromSVG(const Index: TSVGAdobeStockIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGAdobeStock.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+function getBitmapFromSVG(const Index: TSVGBubbleFontIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGBubbleFont.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+procedure RegisterSVGImages;
+begin
+  TSVGInputPrompts.Tag := TOlfSVGBitmapList.AddItem(SVGInputPrompts);
+  TSVGAdobeStock.Tag := TOlfSVGBitmapList.AddItem(SVGAdobeStock);
+  TSVGBubbleFont.Tag := TOlfSVGBitmapList.AddItem(SVGBubbleFont);
+end;
+
+initialization
+
+RegisterSVGImages;
+
+end.
